ef3759788c3d049668bf5d61ca93f6cc1346de23
rulasg <rulasg@outlook.com>
2023-10-21 21:56:11 +0200

    remote script errors on parametrs file

--- ProjectHelperTest/private/parameters.ps1
+++ ProjectHelperTest/private/parameters.ps1
@@ -1,20 +1,24 @@
 # Variables used to the written output of the cmdlets 
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments','',Scope='function')]
 $VerboseParameters =@{
     VerboseAction = 'SilentlyContinue'
     VerboseVariable = 'verboseVar'
 }
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments','',Scope='function')]
 $WarningParameters = @{
     WarningAction = 'SilentlyContinue' 
     WarningVariable = 'warningVar'
 }
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments','',Scope='function')]
 $InfoParameters = @{
     InformationAction = 'SilentlyContinue'
     InformationVariable = 'infoVar'
 }
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments','',Scope='function')]
 $ErrorParameters = @{
     ErrorAction = 'Silently'
     ErrorVariable = 'errorVar'
