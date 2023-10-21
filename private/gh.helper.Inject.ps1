a0bfea9057acc2f787aa0d60d1d78ebc73dde0e3
rulasg <rulasg@outlook.com>
2023-10-22 01:10:02 +0200

    bug when converting null to json

--- private/gh.helper.Inject.ps1
+++ private/gh.helper.Inject.ps1
@@ -27,7 +27,7 @@ function Invoke-GhExpressionToJson{
 
     $result = Invoke-GhExpression -Command $Command
 
-    $ret = $null -eq $result ? $null : $result | ConvertFrom-Json
+    $ret = $null -eq $result ? '[]' : $result | ConvertFrom-Json
 
     return $ret
 }
