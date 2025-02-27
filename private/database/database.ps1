e45b4d5789b3e3630d948fc89373d643298ccb05
rulasg <rulasg@github.com>
2025-02-27 07:10:22 +0100

    Refactor database management by moving database driver to a new file structure

---
+++ private/database/database.ps1
@@ -0,0 +1,50 @@
+# # Database driver to store and cacche project content and schema
+
+
+# $script:PROJECT_DATABASE_LIST = $null
+
+# function Initialize-DatabaseRoot{
+#     [CmdletBinding()]
+#     param()
+    
+#         $script:PROJECT_DATABASE_LIST = @{}
+
+# } Export-ModuleMember -Function Initialize-DatabaseRoot
+
+# function Get-DatabaseRoot{
+#     [CmdletBinding()]
+#     param()
+
+#     if($null -eq $script:PROJECT_DATABASE_LIST){
+#         Initialize-DatabaseRoot
+#     }
+
+#     return $script:PROJECT_DATABASE_LIST
+# }
+
+# function Get-Database{
+#     [CmdletBinding()]
+#     param(
+#         [Parameter(Position = 0)][string]$Owner,
+#         [Parameter(Position = 1)][int]$ProjectNumber
+#     )
+
+#     $root = Get-DatabaseRoot
+
+#     $ret = $root."$owner/$projectnumber"
+
+#     return $ret
+# }
+
+# function Set-Database{
+#     [CmdletBinding()]
+#     param(
+#         [Parameter(Position = 0)][string]$Owner,
+#         [Parameter(Position = 1)][int]$ProjectNumber,
+#         [Parameter(Position = 2)][Object]$Database
+#     )
+
+#     $root = Get-DatabaseRoot
+
+#     $root."$owner/$projectnumber" = $Database
+# }
