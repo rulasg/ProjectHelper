5f4977a7626341b599cb1235cc26183468a53b0f
rulasg <rulasg@github.com>
2025-02-28 07:11:49 +0100

    Refactor tests to remove duplicate Initialize-DatabaseRoot calls and update Get-Project item ID case sensitivity checks

--- Test/public/interactive_test/getproject.test.ps1
+++ Test/public/interactive_test/getproject.test.ps1
@@ -1,15 +1,21 @@
-function Test__Get_Project_ItemId_Equal_Case_Sensitive{
+function Test_Get_Project_ItemId_Equal_Case_Sensitive{
     Reset-InvokeCommandMock
     Initialize-DatabaseRoot
 
     # This project has -Owner github -ProjectNumber 20521 has two items with the same Id case sensitive
-    # PVTI_lADNJr_OALnx2s4Fqq8F
-    # PVTI_lADNJr_OALnx2s4Fqq8f
+    $item1 ="PVTI_lADNJr_OALnx2s4Fqq8F"
+    $item2 ="PVTI_lADNJr_OALnx2s4Fqq8f"
     # Testing that we can load this 
 
-    MockCallJson -Command "Invoke-GitHubOrgProjectWithFields -Owner github -ProjectNumber 20521" -Filename "invoke-GitHubOrgProjectWithFields-github-20521.json"
-    
-    $result = Get-Project -owner github -ProjectNumber 20521 -Verbose
+    # MockCallJson -Command "Invoke-GitHubOrgProjectWithFields -Owner github -ProjectNumber 20521" -Filename "invoke-GitHubOrgProjectWithFields-github-20521.json"
+    MockCallJson -Command "GitHubOrgProjectWithFields" -Filename "invoke-GitHubOrgProjectWithFields-github-20521.json"
 
-    Assert-NotImplemented
+    $result = Get-Project -owner github -ProjectNumber 20521 -Verbose
+    Assert-Count -Expected 86 -Presented $result.items.keys
+
+    $result = Get-ProjectItem  -ItemId $item1
+    Assert-AreEqual -Expected $item1 -Presented $result.id
+
+    $result = Get-ProjectItem -ItemId $item2
+    Assert-AreEqual -Expected $item2 -Presented $result.id
 }
