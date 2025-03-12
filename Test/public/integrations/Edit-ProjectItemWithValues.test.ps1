86a8cfa2074d0601b4916396e7c48c7207e5a20a
rulasg <rulasg@github.com>
2025-03-12 20:36:11 +0100

    Rename Update-ItemWithIntegration function to Update-ProjectItemsWithIntegration and implement its logic in a new file

--- Test/public/integrations/Edit-ProjectItemWithValues.test.ps1
+++ Test/public/integrations/Edit-ProjectItemWithValues.test.ps1
@@ -79,7 +79,7 @@ function Test_UpdateProjectWithIntegration{
         Slug = $fieldSlug
     }
 
-   $result = Update-ItemWithIntegration @param
+   $result = Update-ProjectItemsWithIntegration @param
 
 
     # Confirm that the changes are staged
@@ -98,68 +98,6 @@ function Test_UpdateProjectWithIntegration{
     Assert-AreEqual -Expected $($data2.Number1) -Presented $result.PVTI_lADOAlIw4c4A0Lf4zgYNTxI.PVTF_lADOAlIw4c4A0Lf4zgp2mBs.Value
 }
 
-<#
-.SYNOPSIS
-    Update all the items of a project with an integration command
-.DESCRIPTION
-    Update all the items of a project with an integration command
-    The function will update all the items of a project with the values returned by the integration command
-    The integration command will be called for each Item with the value of the integration field as parameter.
-    The integration command must return a hashtable with the values to be updated
-    The project fields to be updated will have the same name as the hash table keys with a slug as suffix
-    If an item has a field with the name `sf_Name` it will be updated with the value of the hashtable key Name if the slug defined is "sf_"
-.EXAMPLE
-    Update-ItemWithIntegration -Owner "someOwner" -ProjectNumber 164 -IntegrationField "sfUrl" -IntegrationCommand "Get-SfAccount" -Slug "sf_"
-#>
-function Update-ItemWithIntegration{
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0)] [string]$Owner,
-        [Parameter(Position = 1)] [string]$ProjectNumber,
-        [Parameter(Mandatory)][string]$IntegrationField,
-        [Parameter(Mandatory)][string]$IntegrationCommand,
-        [Parameter()] [string]$Slug
-    )
-    ($Owner,$ProjectNumber) = Get-OwnerAndProjectNumber -Owner $Owner -ProjectNumber $ProjectNumber
-    if([string]::IsNullOrWhiteSpace($owner) -or [string]::IsNullOrWhiteSpace($ProjectNumber)){ "Owner and ProjectNumber are required" | Write-MyError; return $null}
-
-    # Get project
-    $project = Get-Project -Owner $owner -ProjectNumber $projectNumber
-
-    # Extract all items that have value on the integration field.
-    # This field is the value that will work as parameter to the integration command
-    $itemList = $project.items.Values | Where-Object { -Not [string]::IsNullOrWhiteSpace($_.$IntegrationField) }
-
-    foreach($item in $itemList){
-        
-        try {
-            $values = Invoke-MyCommand -Command "$IntegrationCommand $($item.$IntegrationField)"
-        }
-        catch {
-            "Something went wrong with the integration command for $($item.id)" | Write-Host -ForegroundColor Red
-        }
-        # Call the ingetration Command with the integration field value as parameter
-
-        # Check if Values is empty or null
-        if($null -eq $values -or $values.Count -eq 0){
-            "No values returned from the integration commandfor $($item.id)" | Write-Host -ForegroundColor Yellow
-            continue
-        }
-
-        # Edit item with the value
-        $param = @{
-            Owner = $owner
-            ProjectNumber = $projectNumber
-            ItemId = $item.id
-            Values = $values
-            FieldSlug = $Slug
-        }
-
-        Edit-ProjectItemWithValues @param
-    }
-
-    
-} 
 
 function Mock_GetProject_Octodemop_625{
 
