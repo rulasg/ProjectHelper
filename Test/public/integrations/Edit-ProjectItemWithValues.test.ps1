b79d973067f6c5a6c2f03c80a345a1262ad9c930
rulasg <rulasg@github.com>
2025-03-12 13:05:12 +0100

    Edit-ProjectItemWithValues implementation and testing

---
+++ Test/public/integrations/Edit-ProjectItemWithValues.test.ps1
@@ -0,0 +1,40 @@
+function Test_EditProjectItemWithValues_Integration{
+
+    # Assert-SkipTest
+    Reset-InvokeCommandMock
+    Mock_DatabaseRoot
+
+    $owner = "octodemo"
+    $projectNumber = "625"
+    $itemId = "PVTI_lADOAlIw4c4A0Lf4zgYNTc0"
+    $fieldSlug = "sf_"
+
+    #MockCallJson -Command "GitHubOrgProjectWithFields" -Filename "invoke-GitHubOrgProjectWithFields-github-20521.json"
+    MockCallJson -Command "Invoke-GitHubOrgProjectWithFields -Owner $owner -ProjectNumber $projectnumber" -Filename "invoke-GitHubOrgProjectWithFields-$owner-$projectNumber.json"
+
+
+    $data = @{
+        "Text1" = "value1"
+        "Text2" = "value2"
+        "Text3" = "value3"
+        "Number1" = "value3"
+    }
+
+    $result = Edit-ProjectItemWithValues -Owner $owner -ProjectNumber $projectNumber -ItemId $itemId -Values $data -FieldSlug $FieldSlug
+
+    # Assert - Confirm update
+    # Assert-IsNull -Object $result
+
+    $result = Get-ProjectItem -Owner $owner -ProjectNumber $projectNumber -ItemId $itemId
+
+    Assert-AreEqual -expected $data.Text1 -Presented $result.$($FieldSlug + "Text1")
+    Assert-AreEqual -expected $data.Text2 -Presented $result.$($FieldSlug + "Text2")
+    # Assert-AreEqual -expected $data.Text3 -Presented $result.$($FieldSlug + "Text3") 
+    Assert-AreEqual -expected $data.Number1 -Presented $result.$($FieldSlug + "Number1")
+
+    # Confirm that the changes are staged
+    $result = Get-ProjectItemStaged -Owner $owner -ProjectNumber $projectNumber
+
+    Assert-Count -Expected 3 -Presented $result.$itemId
+
+}
---
+++ public/integrations/Edit-ProjectItemWithValues.ps1
@@ -0,0 +1,57 @@
+<#
+.SYNOPSIS
+    Update the fields of an item with the Values of a HashTable
+.DESCRIPTION
+    Update the fields of an item with the Values of a HashTable
+    The function will update the fields of the item with the values of the hashtable
+    The hashtable keys must be the field names and the values must be the field values
+#>
+function Edit-ProjectItemWithValues {
+    param (
+        [Parameter(Position = 0)][string]$Owner,
+        [Parameter(Position = 1)][string]$ProjectNumber,
+        [Parameter(Mandatory)][string]$ItemId,
+        [Parameter(Mandatory)][hashtable]$Values,
+        [Parameter()][string]$FieldSlug
+    )
+
+    $fields = Get-ProjectFields -Owner $owner -ProjectNumber $projectNumber
+
+    # forech key in data do
+    foreach ($key in $Values.Keys) {
+        $fieldName = $FieldSlug + $key
+        
+        # Check if field exists 
+        $field = $fields | Where-Object { $_.name -eq $fieldName }
+        if ($null -eq $field) {
+            "Field $fieldName not found" | Write-MyError
+            continue
+        }
+
+        $result = Edit-ProjectItem -Owner $owner -ProjectNumber $projectNumber -ItemId $ItemId -FieldName $fieldName -Value $Values[$key]
+
+        if ($null -eq $result) {
+            "Return to Edit-ProjectItem is not $null" | Write-MyError
+            continue
+        }
+
+    }
+
+} Export-ModuleMember -Function Edit-ProjectItemWithValues
+
+function Update-ItemWithIntegration_POC{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0)] [string]$Owner,
+        [Parameter(Position = 1)] [string]$ProjectNumber
+    )
+    ($Owner,$ProjectNumber) = Get-OwnerAndProjectNumber -Owner $Owner -ProjectNumber $ProjectNumber
+    if([string]::IsNullOrWhiteSpace($owner) -or [string]::IsNullOrWhiteSpace($ProjectNumber)){ "Owner and ProjectNumber are required" | Write-MyError; return $null}
+
+    # Get project
+    $items = Get-ProjectItemList -owner $Owner -ProjectNumber $ProjectNumber
+
+    # List the items that have the integration ID
+    $itemswithIntegration = $items
+    
+} Export-ModuleMember -Function Update-ItemWithIntegration_POC
