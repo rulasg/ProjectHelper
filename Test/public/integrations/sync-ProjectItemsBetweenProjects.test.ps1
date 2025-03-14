64562a9be1cedd39ae1ad5a8713d2ff8ddda0bfb
rulasg <rulasg@github.com>
2025-03-14 07:39:11 +0100

    test wip

--- Test/public/integrations/sync-ProjectItemsBetweenProjects.test.ps1
+++ Test/public/integrations/sync-ProjectItemsBetweenProjects.test.ps1
@@ -4,9 +4,51 @@ function Test_SyncProjectItemsBetweenProjects{
 
     Mock_GetProject_Octodemop_625_626_Sync
 
+    
     $owner = "octodemo"
     $sourceProjectNumber = 625
     $destinationProjectNumber = 626
+    
+    $fieldlist = @("Int1", "Int2")
+
+    $params = @{
+        SourceOwner = $owner
+        SourceProjectNumber = $sourceProjectNumber
+        DestinationOwner = $owner
+        DestinationProjectNumber = $destinationProjectNumber
+        FieldsList = $fieldlist
+    }
+    $result = Sync-ProjectItemsBetweenProjects @params
+
+    Assert-IsNull -Object $result
+
+    $staged = Get-ProjectItemStaged -Owner $owner -ProjectNumber $destinationProjectNumber
+
+    Assert-Count -Expected 3 -Presented $staged.Keys
+    
+    Assert-Count -Expected 2 -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpRY.Keys
+    Assert-AreEqual -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpP0.PVTF_lADOAlIw4c4A0QAozgp6aGw.Value -Expected "Value issue 1"
+    Assert-AreEqual -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpP0.PVTF_lADOAlIw4c4A0QAozgp6aK4.Value -Expected "11"
+
+    Assert-Count -Expected 2 -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpSc.Keys
+    Assert-AreEqual -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpSc.PVTF_lADOAlIw4c4A0QAozgp6aGw.Value -Expected "Value issue 3"
+    Assert-AreEqual -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpSc.PVTF_lADOAlIw4c4A0QAozgp6aK4.Value -Expected "33"
+
+    Assert-Count -Expected 2 -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpP0.Keys
+    Assert-AreEqual -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpRY.PVTF_lADOAlIw4c4A0QAozgp6aGw.Value -Expected "Value issue 2"
+    Assert-AreEqual -Presented $staged.PVTI_lADOAlIw4c4A0QAozgYQpRY.PVTF_lADOAlIw4c4A0QAozgp6aK4.Value -Expected "22"
+
+}
+
+function Test_SyncProjectItemsBetweenProjects_SameValues{
+    Reset-InvokeCommandMock
+    Mock_DatabaseRoot
+
+    Mock_GetProject_Octodemop_625_626_Sync
+
+    $owner = "octodemo"
+    $sourceProjectNumber = 625
+    $destinationProjectNumber = 625
 
     $fieldlist = @("Int1", "Int2")
 
@@ -21,5 +63,5 @@ function Test_SyncProjectItemsBetweenProjects{
 
     $staged = Get-ProjectItemStaged -Owner $owner -ProjectNumber $destinationProjectNumber
 
-    Assert-NotImplemented
+    Assert-Count -Expected 0 -Presented $staged.Keys
 }
