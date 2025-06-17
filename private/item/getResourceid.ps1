6c8b00bbdb9627b3333f778babb3c0ca7bf22c5c
rulasg <rulasg@github.com>
2025-06-17 11:11:19 +0200

    feat(getResourceid): add function to retrieve resource ID from URL

---
+++ private/item/getResourceid.ps1
@@ -0,0 +1,26 @@
+
+
+Set-MyInvokeCommandAlias -Alias GetIssueOrPullRequest -Command 'Invoke-GetIssueOrPullRequest -Url {url}'
+
+
+function Get-ResourceIdFromUrl{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position = 0)][string]$Url
+    )
+
+    $params = @{
+        url = $Url
+    }
+
+    $response = Invoke-MyCommand -Command GetIssueOrPullRequest -Parameters $params
+
+    if($response.data.resource){
+        $ret = $response.data.resource.id
+    } else {
+        "Resource not found for URL [$Url]" | Write-MyError
+        $ret = $null
+    }
+
+    return $ret
+} Export-ModuleMember -Function Get-ResourceIdFromUrl
