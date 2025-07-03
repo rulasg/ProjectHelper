fa707ba2d8a1591d79c5fa8556070300a324de6b
rulasg <rulasg@github.com>
2025-07-03 07:49:44 +0200

    refactor(prompt): improve prompt handling and integration with posh-git

--- public/getPrompt.ps1
+++ public/getPrompt.ps1
@@ -15,7 +15,7 @@ function Get-ProjecthelperPrompt{
     
     # Get Staged items
     $db = Get-ProjectFromDatabase -Owner $Owner -ProjectNumber $ProjectNumber
-    $count = $db.Staged.Values.count
+    $count = $db.Staged.Values.Values.Count
 
     # Build prompt text
     $prompt = "[$owner/$projectNumber/$count]"
@@ -34,26 +34,43 @@ function Set-ProjecthelperPrompt{
         [switch]$WithNewLine
     )
 
-    if($GitPromptSettings){
-        "Setting Prompt with posh-git integration" | Write-Host
-        if($WithNewLine){
-            $GitPromptSettings.DefaultPromptBeforeSuffix.Text ='$(Get-ProjecthelperPrompt -WithNewLine)' + $GitPromptSettings.DefaultPromptBeforeSuffix.Text
-        } else {
-            $GitPromptSettings.DefaultPromptBeforeSuffix.Text ='$(Get-ProjecthelperPrompt)' + $GitPromptSettings.DefaultPromptBeforeSuffix.Text
-        }
 
-        $GitPromptSettings.DefaultPromptBeforeSuffix.ForegroundColor = 'DarkYellow'
+    $prompt = @'
+$( #PROJECTHELPER
+    $testcmd = (Get-Command -name 'Get-ProjecthelperPrompt' -ErrorAction SilentlyContinue) -eq $null
+    if(-not $testcmd){
+        $prompt = Get-ProjecthelperPrompt -WithNewLine:${withnewline}
+    } else {
+        $prompt = [string]::Empty
+    }
+    $prompt
+#PROJECTHELPER_END
+)
+'@
+
+    if($GitPromptSettings){
+        
+        # Check if the prompt is already part of the DefaultPromptBeforeSuffix.Text
+        if (-not $GitPromptSettings.DefaultPromptBeforeSuffix.Text.Contains('#PROJECTHELPER')) {
+            "Setting Prompt with posh-git integration" | Write-Host
+            $GitPromptSettings.DefaultPromptBeforeSuffix.ForegroundColor = 'DarkYellow'
+            $prompt = $prompt -replace '{withnewline}', $WithNewLine.ToString()
+            $prompt | Write-Verbose
+            # Only add our prompt if it's not already there
+            $GitPromptSettings.DefaultPromptBeforeSuffix.Text = $prompt + $GitPromptSettings.DefaultPromptBeforeSuffix.Text
+        }
+        else {
+            "ProjectHelper prompt already configured in posh-git" | Write-Host
+        }
 
         return
     }
 
     # Default prompt setup
     "Setting Projecthelper Default Prompt" | Write-Host
-    if($WithNewLine){
-        $function:prompt = { "$(Get-ProjecthelperPrompt -WithNewLine) $($ExecutionContext.SessionState.Path.CurrentLocation)> " }
-    } else {
-        $function:prompt = { "$(Get-ProjecthelperPrompt) $($ExecutionContext.SessionState.Path.CurrentLocation)> " }
-    }
+    $prompt = '$($ExecutionContext.SessionState.Path.CurrentLocation.Path) + " " + $(Get-ProjecthelperPrompt -WithNewLine:${withnewline})'
+    $prompt = $prompt -replace '{withnewline}', $WithNewLine.ToString()
+    $prompt | Write-Verbose
+    $function:prompt = [scriptblock]::Create($prompt)
 
 } Export-ModuleMember -Function Set-ProjecthelperPrompt
-
