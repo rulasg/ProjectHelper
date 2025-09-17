bd3c0b79fb44487a955dc2d5565ec47fc8b2e9f4
Raúl (Dibildos) González <rulasg@github.com>
2025-09-17 15:07:59 +0200

    fix(Sync-ProjectDatabase): add -Safe parameter to Save-ProjectDatabase to prevent overwriting changes

--- private/projectDatabase/project_database_Sync.ps1
+++ private/projectDatabase/project_database_Sync.ps1
@@ -20,7 +20,7 @@ function Sync-ProjectDatabase{
     # Send update to project
     $db = Sync-Project -Database $db
 
-    Save-ProjectDatabase -Database $db
+    Save-ProjectDatabase -Database $db -Safe
 
     if (Test-ProjectDatabaseStaged -Owner $Owner -ProjectNumber $ProjectNumber) {
         "Still pending staged values" | Write-MyError
