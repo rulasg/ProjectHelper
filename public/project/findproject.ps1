91021935af400494514cfc0a9e434dc6ab1e4495
Raúl (Dibildos) González <rulasg@github.com>
2025-09-29 11:42:17 +0200

    docs(findproject): update documentation for Find-Project function. Enhance output

--- public/project/findproject.ps1
+++ public/project/findproject.ps1
@@ -1,11 +1,28 @@
 Set-MyInvokeCommandAlias -Alias FindProject -Command 'Invoke-FindProject -Owner {owner} -Pattern "{pattern}" -firstProject {firstProject} -afterProject "{afterProject}"'
+<#
+.SYNOPSIS
+    Find a project by name pattern
+.DESCRIPTION
+    Find a project using a search pattern used on GitHub UI.
+.EXAMPLE
+    Find-Project -owner githubcustomers -pattern creator:rulasg
+    Find all projects in the organization githubcustomers created by user rulasg
+.EXAMPLE
+    Find-Project -owner octodemo -pattern "development"
+    Find all projects in the organization octodemo with "development" in the title of the project
 
+.NOTES
+    Reference: https://docs.github.com/en/issues/planning-and-tracking-with-projects/finding-your-projects#syntax-for-filtering-a-list-of-projects
+#>
 function Find-Project{
     [CmdletBinding()]
     param(
         [Parameter()][string]$Owner,
         [Parameter()][string]$Pattern
     )
+
+    # Seaarch syntax: https://docs.github.com/en/issues/planning-and-tracking-with-projects/finding-your-projects#syntax-for-filtering-a-list-of-projects
+
 
     $params = @{
         owner = $Owner
@@ -18,12 +35,12 @@ function Find-Project{
 
     $projects = $result.data.organization.projectsV2.nodes
 
-    if(-not $projects){
+    if($null -eq $projects){
         "Error finding projects for owner [$Owner] with pattern [$Pattern]" | Write-MyError
         return $null
-    }
+    } 
 
-    "Verbose found [$($projects.Count)] projects for owner [$Owner] with pattern [$Pattern]" | Write-MyVerbose
+    "[$($projects.Count)] projects found" | Write-MyHost
 
     return $projects
 
