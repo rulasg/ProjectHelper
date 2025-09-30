05d0cdbc00ba421b4356302a9f02a2eb71b46449
Raúl (Dibildos) González <rulasg@github.com>
2025-09-30 14:30:00 +0200

    feat(AddComment): implement async add comment functionality for pull requests

--- private/projectDatabase/project_database_call.ps1
+++ private/projectDatabase/project_database_call.ps1
@@ -213,23 +213,54 @@ function Update-PullRequest {
         [Parameter()][switch]$Async
     )
 
-    "Calling to update PullRequest Async[$Async] [$Id/$FieldId = ""$Value"" ]" | Write-MyHost
+    "Calling to update Issue Async[$Async] [$Id/$FieldId = ""$Value"" ]" | Write-MyHost
 
-    $params = @{
-        id    = $Id
-        title = if ($FieldId -eq "title") { $Value } else { "" }
-        body  = if ($FieldId -eq "body") { $Value } else { "" }
+    if($FieldId -eq "addcomment"){
+        # ADD COMMENT
+
+        if([string]::IsNullOrWhiteSpace($Value)){
+            "Comment value is empty" | Write-MyWarning
+            return $null, $null, "addComment"
+        } 
+
+         $params =  @{
+                subjectid = $Id
+                comment   = $Value
+         }
+         
+        if ($Async) {
+            $params.projecthelper = $MODULE_PATH
+            $job = Start-MyJob -Command AddCommentAsync -Parameters $params
+
+        }
+        else {
+            $result = Invoke-MyCommand -Command AddComment -Parameters $params
+        }
+
+        $returnType = "addComment"
+    
+    } else {
+        # TITLE AND BODY
+        
+        $params = @{
+            id    = $Id
+            title = if ($FieldId -eq "title") { $Value } else { "" }
+            body  = if ($FieldId -eq "body") { $Value } else { "" }
+        }
+        
+        if ($Async) {
+            $params.projecthelper = $MODULE_PATH
+            $job = Start-MyJob -Command UpdatePullRequestAsync -Parameters $params
+            
+        }
+        else {
+            $result = Invoke-MyCommand -Command UpdatePullRequest -Parameters $params
+        }
+
+        $returnType = "updatePullRequest"
     }
 
-    if ($Async) {
-        $params.projecthelper = $MODULE_PATH
-        $job = Start-MyJob -Command UpdatePullRequestAsync -Parameters $params
-    }
-    else {
-        $result = Invoke-MyCommand -Command UpdatePullRequest -Parameters $params
-    }
-
-    return $result, $job , "updatePullRequest"
+    return $result, $job, $returnType
 }
 
 function Update-DraftIssue {
