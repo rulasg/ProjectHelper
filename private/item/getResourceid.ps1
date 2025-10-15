9be5680b54709107a265868ad57eac2c9bd51fb9
Raúl (Dibildos) González <rulasg@github.com>
2025-10-15 09:22:13 +0100

    refactor(getResourceid): rename function to Get-ContentIdFromUrlDirect

--- private/item/getResourceid.ps1
+++ private/item/getResourceid.ps1
@@ -3,7 +3,7 @@
 Set-MyInvokeCommandAlias -Alias GetIssueOrPullRequest -Command 'Invoke-GetIssueOrPullRequest -Url {url}'
 
 
-function Get-ContentIdFromUrl{
+function Get-ContentIdFromUrlDirect{
     [CmdletBinding()]
     param(
         [Parameter(Mandatory,Position = 0)][string]$Url
@@ -23,4 +23,4 @@ function Get-ContentIdFromUrl{
     }
 
     return $ret
-} Export-ModuleMember -Function Get-ContentIdFromUrl
+} Export-ModuleMember -Function Get-ContentIdFromUrlDirect
