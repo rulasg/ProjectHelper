96af1d0e831eeba4280aa19589e37686a5144d2f
Raúl (Dibildos) González <rulasg@github.com>
2025-10-20 16:38:12 +0200

    feat(repository): implement caching for Get-Repository function

--- public/repository/get-repository.ps1
+++ public/repository/get-repository.ps1
@@ -4,8 +4,19 @@ Set-MyInvokeCommandAlias -Alias GetRepository -Command "Invoke-Repository -Owner
 function Get-Repository{
     param(
         [Parameter(Mandatory)][string]$Owner,
-        [Parameter(Mandatory)][string]$Name
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter()][switch]$Force
     )
+
+    # Check database
+    $key = "$Owner-$Name"
+    $repodb = Get-Database -Key $key
+
+    # use cache if available and not forced
+    if( $repodb -and (! $Force) ){
+         # Get repository from GitHub
+         return $repodb
+    }
 
     $params = @{
         owner = $Owner
@@ -19,9 +30,16 @@ function Get-Repository{
         throw "Repository $Owner/$Name not found"
     }
 
+    # Transformations
     $repo.owner = $repo.owner.login
+    # This parent comes from the query justin case the repo is a fork show the parent where to crete issues.
+    # We will ignore this by the moment as we do not use forks normally
+    $repo.PSObject.Properties.Remove('parent')
 
     $ret = $repo
+
+    # Save to database
+    Save-Database -Key $key -Database $ret
 
     return $ret
 
