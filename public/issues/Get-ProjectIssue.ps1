131cbc170d9782303c7ced6189b5bf398a899648
Raúl (Dibildos) González <rulasg@github.com>
2025-10-26 15:39:27 +0100

    feat(issue): Get-ProjectIssue

--- public/issues/Get-ProjectIssue.ps1
+++ public/issues/Get-ProjectIssue.ps1
@@ -5,17 +5,28 @@ function Get-ProjectIssue {
         [Parameter(Position=0)][string]$Url
     )
 
-    # Check the cache of the default project
-
+    # Check the project cache of the default project
     $owner,$projectNumber = Get-OwnerAndProjectNumber
+    $cache = Get-ProjectItemByUrl -Owner $owner -ProjectNumber $projectNumber -Url $Url
+    if( $cache ) { return $cache }
 
-    $cache = Search-ProjectItem -Owner $owner -ProjectNumber $projectNumber -FieldName "url" -Filter $Url -Exact
-
-    if( $cache ) {
-        return $cache
+    # Find project
+    $params = @{
+        url = $Url
     }
 
-    $issue = Get-ProjectIssueDirect -Url $Url
+    $response = Invoke-MyCommand -Command GetIssueOrPullRequest -Parameters $params
 
-    return $issue
+    if($response.data.resource){
+        $ret = $response.data.resource
+    } else {
+        "Resource not found for URL [$Url]" | Write-MyError
+        $ret = $null
+    }
+
+    # TODO: update the cache if issue is an item of the project
+    # This will required or transform Issue to Item or create a new issue database
+
+    return $ret
+
 } Export-ModuleMember -Function Get-ProjectIssue
