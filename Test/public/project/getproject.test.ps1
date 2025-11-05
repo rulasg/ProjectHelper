e92d5554cae86fe3d446403cd8daff1bed940f3e
Raúl (Dibildos) González <rulasg@github.com>
2025-11-05 20:37:57 +0100

    wip

--- Test/public/project/getproject.test.ps1
+++ Test/public/project/getproject.test.ps1
@@ -2,13 +2,15 @@ function Test_GetProject_With_Query{
     Reset-InvokeCommandMock
     Mock_DatabaseRoot
 
-    $p = Get-Mock_Project_700 ; $owner = $p.owner ; $projectNumber = $p.projectNumber
+    $p = Get-Mock_Project_700 ; $owner = $p.owner ; $projectNumber = $p.number
     $i = $p.issue
     $query = "some query"
     
-    MockCall_GitHubOrgProjectWithFields -Owner $owner -ProjectNumber $projectNumber -Query $query
+    # MockCall_GitHubOrgProjectWithFields -Owner $owner -ProjectNumber $projectNumber -Query $query -Fieldname
 
-    $result = Get-Project
+    $result = Invoke-GitHubOrgProjectWithFields -Owner $owner -ProjectNumber $projectNumber -query $query
+
+    $result = Update-ProjectDatabase -Owner $owner -ProjectNumber $projectNumber -Query $query
 
     Assert-NotImplemented
     
