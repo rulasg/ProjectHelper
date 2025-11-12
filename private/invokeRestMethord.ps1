32d131d2dfe532c9c2b00b9ad1d565c73719505e
Raúl (Dibildos) González <rulasg@github.com>
2025-11-12 09:22:38 +0100

    refactor(invokeRestMethod): comment out function implementation for clarity

--- private/invokeRestMethord.ps1
+++ private/invokeRestMethord.ps1
@@ -1,29 +1,29 @@
 
-function Invoke-RestMethod{
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0)][string]$Method,
-        [Parameter(Position = 1)][string]$Uri,
-        [Parameter(Position = 2)][hashtable]$Headers,
-        [Parameter(Position = 3)][string]$Body,
-        [Parameter()][string]$OutFile
-    )
+# function Invoke-RestMethod{
+#     [CmdletBinding()]
+#     param(
+#         [Parameter(Position = 0)][string]$Method,
+#         [Parameter(Position = 1)][string]$Uri,
+#         [Parameter(Position = 2)][hashtable]$Headers,
+#         [Parameter(Position = 3)][string]$Body,
+#         [Parameter()][string]$OutFile
+#     )
 
-    $params = @{
-        Method = $Method
-        Uri = $Uri
-        Headers = $Headers
-        Body = $Body
-    }
+#     $params = @{
+#         Method = $Method
+#         Uri = $Uri
+#         Headers = $Headers
+#         Body = $Body
+#     }
     
-    if (-not [string]::IsNullOrWhiteSpace($OutFile)) {
-        $params.OutFile = $OutFile
-    }
+#     if (-not [string]::IsNullOrWhiteSpace($OutFile)) {
+#         $params.OutFile = $OutFile
+#     }
 
-    ">> $Method $Uri" | Write-MyDebug -section "invokeRestMethod" 
-    $result = Microsoft.PowerShell.Utility\Invoke-RestMethod @params
-    "<< $Method $Uri" | Write-MyDebug -section "invokeRestMethod" 
+#     ">> $Method $Uri" | Write-MyDebug -section "invokeRestMethod" 
+#     $result = Microsoft.PowerShell.Utility\Invoke-RestMethod @params
+#     "<< $Method $Uri" | Write-MyDebug -section "invokeRestMethod" 
 
 
-    return $result
-}
+#     return $result
+# }
