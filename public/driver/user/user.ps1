a09205ea9166e017b5bf5cdb9a6dde40a80a01d7
Raúl (Dibildos) González <rulasg@github.com>
2025-11-12 09:22:19 +0100

    feat(user): add Invoke-GetUser and Get-User functions for user retrieval

---
+++ public/driver/user/user.ps1
@@ -0,0 +1,35 @@
+
+Set-MyinvokeCommandAlias -Alias getUser -Command "Invoke-GetUser -Handle {handle}"
+function Invoke-GetUser{
+    param(
+        [Parameter(Mandatory)][string]$Handle
+    )
+
+     $result = Invoke-RestAPI -Api /users/$Handle
+
+     return $result
+
+} Export-ModuleMember -Function Invoke-GetUser
+
+function Get-User{
+    param(
+        [Parameter(Mandatory)][string]$Handle,
+        [Parameter()][switch]$Force
+    )
+
+    $key = "user-$Handle"
+
+    # Check cache
+    $cache = Get-Database -Key $key
+    if($force -or ($null -ne $cache)){
+        return $cache
+    }
+
+     $result = Invoke-MyCommand -Command "getUser" -Parameters @{handle=$Handle}
+
+     # Cache
+     Save-Database -Key "user-$Handle" -Database $result
+
+     return $result
+
+} Export-ModuleMember -Function Get-User
