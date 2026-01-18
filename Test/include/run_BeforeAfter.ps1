30b48fee86087445a3d21b53a5dcb107564aedd9
Raúl (Dibildos) González <rulasg@github.com>
2026-01-18 22:45:04 +0100

    feat(includeFiles): add functions to run before and after tests

---
+++ Test/include/run_BeforeAfter.ps1
@@ -0,0 +1,26 @@
+# Run Before and After any test
+# 
+# Supported by TestingHelper 4.1.0 we can specify code that will run :
+# - Before each test
+# - After each test
+# - Before all tests
+# - After all tests
+
+function Run_BeforeAll{
+    Write-Verbose "Run_BeforeAll"
+}
+
+function Run_AfterAll{
+    Write-Verbose "Run_AfterAll"
+}
+
+function Run_BeforeEach{
+    Write-Verbose "Run_BeforeEach"
+}
+
+function Run_AfterEach{
+    Write-Verbose "Run_AfterEach"
+}
+
+Export-ModuleMember -Function Run_*
+
