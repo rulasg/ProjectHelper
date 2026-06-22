0d88d2b217e5c5c8d605c459859b034b945a66ef
Raúl (Dibildos) González <rulasg@github.com>
2026-06-22 10:42:27 +0200

    test(user): implement GetUser test with mock data for user 'mona'

--- Test/public/driver/user/user.test.ps1
+++ Test/public/driver/user/user.test.ps1
@@ -1,4 +1,13 @@
 function Test_GetUser_SUCCESS{
 
-    Assert-NotImplemented
+    MockCallJson -Command "Invoke-GetUser -Handle mona" -filename "invoke-GetUser-rulasg.json"
+
+    # act
+    $result = Get-User -Handle "mona"
+
+    # Assert
+    Assert-AreEqual -Expected "Mona Octocat" -Presented $result.Name
+    Assert-AreEqual -Expected "mona" -Presented $result.Login
+    Assert-AreEqual -Expected "MDQ6VXNlcjY4ODQ0MDg=" -Presented $result.Id
+    Assert-AreEqual -Expected "mona@github.com" -Presented $result.Email
 }
